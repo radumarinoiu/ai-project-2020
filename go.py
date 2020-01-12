2f5f12b2b197b435d96e0dcdfa199558e2d592c1
7-sphere <silvan.reiner@gmail.com>
2020-01-12 16:29:12 +0200

    added state class

--- go.py
+++ go.py
@@ -1,46 +1,66 @@
 import numpy as np, random, queue, abc
 
-class Game:
-    def __init__(self, agent_one, agent_two, boardsize = 19):
+class State:
+    def __init__(self, boardsize, board, active_player, empty_fields, passed, previous_state):
         self.boardsize = boardsize
-        self.board = np.zeros((boardsize, boardsize))
-        self.agent_one = agent_one
-        self.agent_two = agent_two
-        self.active_player = 1
-        self.empty_fields = boardsize*boardsize
-        self.history = [[self.empty_fields, self.board]]
-        self.passed = False
+        self.board = board.copy()
+        self.active_player = active_player
+        self.empty_fields = empty_fields
+        self.passed = passed
+        self.previous_state = previous_state
+        self.possible_moves = []
+        self.finished = False
+        
+    def add_possible_moves(self):
+        if len(self.possible_moves) > 0:
+            return
+        self.add_result_state(True, 0, 0)
+        for i in range(self.boardsize):
+            for j in range(self.boardsize):
+                self.add_result_state(False, i, j)
+                
+    def add_result_state(self, passes, x, y):
+        result = self.result_state(self, passes, x, y)
+        if result != None:
+            self.possible_moves.append(result)
     
-    def print_state(self): #this just blurts emojis into to the console, should this be improved?
-        for i in range(self.boardsize + 2):
-            print('\N{Black Square Button}', end = '')
-        print('')
-        for row in self.board:
-            print('\N{Black Square Button}', end = '')
-            for field in row:
-                if field == 0:
-                    print('\N{IDEOGRAPHIC SPACE}', end = '')
-                elif field == 1:
-                    print('\N{Medium Black Circle}', end = '')
-                elif field == -1:
-                    print('\N{Medium White Circle}', end = '')
-            print('\N{Black Square Button}')
-        for i in range(self.boardsize + 2):
-            print('\N{Black Square Button}', end = '')
-        print('')
-        print('')
+    def result_state(self, passes, x, y):
+        if self.board[x,y] != 0:
+            return None
+        new_state = State(self.boardsize, self.board, self.active_player, self.empty_fields, self.passed, self)
+        if new_state.make_move(passes, x, y):
+            return new_state
+        return None
         
-    def winner(self):
-        score = self.score()
-        if score == 0:
-            return 0
-        elif score > 0:
-            return 1
+    def make_move(self, passes, x, y):
+        if passes and self.passed:
+            self.finished = True
+        elif self.board[x,y] == 0:
+            self.board[x, y] = self.active_player
+            self.empty_fields += self.remove_all_encircled(x, y)-1
+            if not self.move_is_ko_legal(x, y):
+                return False
         else:
-            return -1
-        
-    def move_is_legal(self, x_coord, y_coord): #TODO: Ko rule
-        return self.board[x_coord, y_coord] == 0
+            return False
+        self.passed = passes
+        self.active_player = -self.active_player
+        return True
+    
+    def move_is_ko_legal(self, x_coord, y_coord):
+        checked_state = self.previous_state
+        while checked_state != None:
+            if checked_state.empty_fields == self.empty_fields:
+                if self.board_is_equal(checked_state.board):
+                    return False
+            checked_state = checked_state.previous_state
+        return True
+    
+    def board_is_equal(self, test_board):
+        for i in range(self.boardsize):
+            for j in range(self.boardsize):
+                if self.board[i,j] != test_board[i,j]:
+                    return False
+        return True
     
     def remove_all_encircled(self, x_coord, y_coord):
         colour = self.board[x_coord, y_coord]
@@ -105,41 +125,71 @@ class Game:
             for y in range(self.boardsize):
                 colour, group = self.encircled(checked, (x, y), 0)
                 score += colour*len(group)+self.board[x,y]
-        return score - 6.5
+        return score - 6.5  
+    
+    def print_state(self): #this just blurts emojis into to the console, should this be improved?
+        for i in range(self.boardsize + 2):
+            print('\N{Black Square Button}', end = '')
+        print('')
+        for row in self.board:
+            print('\N{Black Square Button}', end = '')
+            for field in row:
+                if field == 0:
+                    print('\N{IDEOGRAPHIC SPACE}', end = '')
+                elif field == 1:
+                    print('\N{Medium Black Circle}', end = '')
+                elif field == -1:
+                    print('\N{Medium White Circle}', end = '')
+            print('\N{Black Square Button}')
+        for i in range(self.boardsize + 2):
+            print('\N{Black Square Button}', end = '')
+        print('')
+        print('')
+        
+    def clear_possible_moves(self):
+        self.possible_moves = []
+    
+
+class Game:
+    def __init__(self, agent_one, agent_two, boardsize = 19):
+        self.state = State(boardsize, np.zeros((boardsize, boardsize)), 1, boardsize*boardsize, False, None)
+        self.agent_one = agent_one
+        self.agent_two = agent_two
+    
+    def winner(self):
+        score = self.state.score()
+        if score == 0:
+            return 0
+        elif score > 0:
+            return 1
+        else:
+            return -1
         
     def run_game(self):
-        while True:
-            legal = False
-            while not legal:
-                if self.active_player == 1:
-                    passes, x, y = self.agent_one.move(self)
+        while not self.state.finished:
+            result = None
+            while result == None:
+                if self.state.active_player == 1:
+                    passes, x, y = self.agent_one.move(self.state)
                 else:
-                    passes, x, y = self.agent_two.move(self)
-                if passes:
-                    legal = True
-                else:
-                    legal = self.move_is_legal(x, y)
-            if self.passed and passes:
-                return self.winner()
-            self.passed = passes
-            if not passes:
-                self.board[x, y] = self.active_player
-                self.empty_fields += self.remove_all_encircled(x, y)-1
-            self.active_player = -self.active_player
-            self.history.append([self.empty_fields, self.board])
-            self.print_state()
-            print(self.score())
+                    passes, x, y = self.agent_two.move(self.state)
+                result = self.state.result_state(passes, x, y)
+            self.state.clear_possible_moves()
+            self.state = result
+            self.state.print_state()
+            print(self.state.score())
+        print(self.winner())
 
 class Agent(abc.ABC):
-    def move(self, board):
+    def move(self, state):
         pass
 
-#game with RandomAgents will never end because they will never pass a turn
+#game with RandomAgents will  end because they will never pass a turn
 class RandomAgent(Agent):
-    def move(self, game):
-        return False, random.randrange(game.boardsize), random.randrange(game.boardsize)
+    def move(self, state):
+        return False, random.randrange(state.boardsize), random.randrange(state.boardsize)
 
-#TODO: MinMax Agent as first step towards something similar to AlphaGo?
+#TODO: Monte Carlo Tree Search Agent as first step towards something similar to AlphaGo?
 
 test = Game(RandomAgent(), RandomAgent())
 test.run_game()
